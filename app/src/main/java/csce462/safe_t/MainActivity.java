5cd6798333e1a4078e4268cd0b9fefd462913fc0
Sarah <sarah.beardsley21@yahoo.com>
2017-03-27 13:36:53 -0500

    initial UI

---
+++ app/src/main/java/csce462/safe_t/MainActivity.java
@@ -0,0 +1,19 @@
+package csce462.safe_t;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the Edit Contacts button */
+    public void editContacts(View view) {
+
+    }
+}
